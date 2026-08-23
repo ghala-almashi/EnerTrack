3a70365876e7bc44e3e98a98c87a334018d97db1
Samar Alsaidi <s445005627@uqu.edu.sa>
2026-08-23 11:50:45 +0300

    update

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('dashboard/', dashboard_view, name='dashboard'),
     path('dashboard/', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
